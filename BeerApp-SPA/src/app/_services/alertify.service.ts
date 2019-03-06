fe8531f31fe82b1f7a7b5d1ff9f858850b8e5f0f
Tim Lukens <tblukens87@gmail.com>
2019-03-06 14:06:09 -0600

    Display users name on nav welcome.

--- BeerApp-SPA/src/app/_services/alertify.service.ts
+++ BeerApp-SPA/src/app/_services/alertify.service.ts
@@ -8,7 +8,7 @@ export class AlertifyService {
   constructor() {}
 
   confirm(message: string, okCallback: () => any) {
-    alertify.confirm(message, function(e) {
+    alertify.confirm(message, e => {
       if (e) {
         okCallback();
       } else {
