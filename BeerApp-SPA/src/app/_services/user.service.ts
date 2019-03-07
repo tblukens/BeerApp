27a0fe85d594b4ca3a48e0ba89c83c494ac9da7d
Tim Lukens <tblukens87@gmail.com>
2019-03-07 00:47:39 -0600

    Members edit functions

--- BeerApp-SPA/src/app/_services/user.service.ts
+++ BeerApp-SPA/src/app/_services/user.service.ts
@@ -17,6 +17,10 @@ export class UserService {
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>((this.baseUrl + 'users/' + id));
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 }
